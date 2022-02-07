99c8a2ecbd725bb3ad341dcaec72be33bad65108
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-02-07 22:42:05 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import NextApp from 'next/app'
 import '@the-innovation-circuit/theme/fonts/fonts.css'
 import theme from '@the-innovation-circuit/theme'
 import { ThemeProvider } from 'theme-ui'
+import Script from 'next/script'
 
 export default class App extends NextApp {
   render() {
@@ -14,6 +15,7 @@ export default class App extends NextApp {
           colors: { ...theme.colors, modes: {}, primary: '#ff686b', green: 'rgb(109, 254, 104)' }
         }}
       >
+        <Script data-website-id="282db514-17c9-4875-a705-e758f74810fb" src="https://analytics.sampoder.com/umami.js" />
         <Component {...pageProps} />
       </ThemeProvider>
     )
